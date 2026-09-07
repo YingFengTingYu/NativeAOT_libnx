6fbf29366f882700fb7b86d152e21a963337e422
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-09-07 18:09:50 +0900

    Build breaks

--- src/coreclr/System.Private.CoreLib/src/System/Reflection/RuntimeCustomAttributeData.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Reflection/RuntimeCustomAttributeData.cs
@@ -1836,7 +1836,7 @@ namespace System.Reflection
             if (reader is null)
                 return false;
 
-            RuntimeType.ListBuilder<object> derivedAttributes = default;
+            ListBuilder<object> derivedAttributes = default;
             foreach (CustomAttributeHandle customAttributeHandle in customAttributeHandles)
             {
                 if (FilterCustomAttributeRecord(
@@ -1858,7 +1858,7 @@ namespace System.Reflection
             MetadataReader reader,
             RuntimeType attributeFilterType,
             bool mustBeInheritable,
-            ref RuntimeType.ListBuilder<object> derivedAttributes)
+            ref ListBuilder<object> derivedAttributes)
         {
             CustomAttribute customAttribute = customAttributeHandle.GetCustomAttribute(reader);
             Handle attributeTypeHandle = customAttribute.GetAttributeTypeHandle(reader);
@@ -2082,12 +2082,12 @@ namespace System.Reflection
             Justification = "Linker guarantees presence of all the constructor parameters, property setters and fields which are accessed by any " +
                             "attribute instantiation which is present in the code linker has analyzed.")]
         private static void AddCustomAttributes(
-            ref RuntimeType.ListBuilder<object> attributes,
+            ref ListBuilder<object> attributes,
             MetadataReader? reader,
             CustomAttributeHandleCollection customAttributeHandles,
             RuntimeType? attributeFilterType,
             bool mustBeInheritable,
-            RuntimeType.ListBuilder<object> derivedAttributes)
+            ListBuilder<object> derivedAttributes)
         {
             if (reader is null)
                 return;
